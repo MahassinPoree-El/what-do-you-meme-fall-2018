3a130f55dc24a00ccb5abcb605840ce82f2e7c02
MahassinPoree-El <poreeelm1@hawkmail.newpaltz.edu>
2018-11-01 11:48:35 -0400

    sommore classwork

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -7,20 +7,17 @@
     <div class="row">
         <div class="col-md-4">
             <div class="card" >
-                <div class="card-body">
-                    <h5 class="card-title">Players</h5>
+                    <h5 class="card-header">Players</h5>
                     <ul class="list-group list-group-flush">
-                        <li v-for="p in state.players" class="list-group-item">{{p}}</li>
+                        <li v-for="p in state.players" :key="p.id"
+                            class="list-group-item">{{p.name}}
+                            <span class="badge badge-primary badge-pill">{{p.score}}</span>
+                        </li>
                     </ul>
-                </div>
+                
             </div>
             <div class="card" >
-                <div class="card-body">
-                    <h5 class="card-title">My Captions</h5>
-                    <ul class="list-group list-group-flush">
-                        <li v-for="c in myCaptions" class="list-group-item">{{c}}</li>
-                    </ul>
-                  </div>
+                    <h5 class="card-header">My Captions</h5>
             </div>
         </div>
         <div class="col-md-4">
@@ -32,11 +29,10 @@
         </div>
         <div class="col-md-4">
             <div class="card" >
-                <div class="card-body">
-                    <h5 class="card-title">Played Captions</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
+                    <h5 class="card-header">Played Captions</h5>
+                    <ul class="list-group list-group-flush">
+                        <li v-for="c in stat.playedCaptions" :key="c.text" class="list-group-item">{{c}}</li>
+                    </ul>
             </div>
         </div>
     </div>
@@ -44,6 +40,18 @@
 </template>
 
 <style lang="scss">
+    li.list-group-item {
+        display: flex;
+        align-content: center;
+        img{
+            width: 30px; height: 30px;
+            margin-right: 5px;
+        }
+
+        h5{
+            flex-grow: 1;
+        }
+    }
 </style>
 
 <script>
