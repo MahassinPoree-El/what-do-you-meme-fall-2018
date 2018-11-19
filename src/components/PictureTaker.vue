498ea28a32c35796037c8e5136b6f4832c43163a
MahassinPoree-El <poreeelm1@hawkmail.newpaltz.edu>
2018-11-19 12:00:44 -0500

    created capture

--- src/components/PictureTaker.vue
+++ src/components/PictureTaker.vue
@@ -18,15 +18,18 @@ export default {// is a vue object
     },
     methods:
     {
-        capture()
+        capture() //for animation
         {
-
+            this.canvas.getContext("2d").drawImage(this.video, 0, 0, 640, 480);
+            this.captures.push(this.canvas.toDataURL("image/png"));
         }
     },
     mounted()
     {
         this.video = this.$refs.video;   //document.getElementById("video"); //uses the video ID uptop
         this.canvas = this.$refs.canvas;
+        this.conext2d = this.canvas.getContext("2d");
+
         navigator.mediaDevices.getUserMedia({ video: true, audio: false })
         .then(stream => {
             this.video.src = window.URL.createObjectURL(stream);//THIS IS A PROMISE
